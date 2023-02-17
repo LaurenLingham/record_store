018116df4ac5270e33bd275e32feb0adc4f27eae
LaurenLingham <lauren.lingham@outlook.com>
2023-02-17 14:52:25 +0000

    added new album functions

--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -35,17 +35,6 @@ def select(id):
         album = Album(artist, result["title"], result["year_released"], result["genre"], result["stock_qty"], result["purchase_price"], result["sell_price"], result["id"])
     return album
 
-def filter_by_artist(id):
-    albums = []
-    sql = "SELECT * FROM albums WHERE artist_id = %s"
-    values = [id]
-    results = run_sql(sql, values)
-    for row in results:
-        artist = artist_repository.select(row["artist_id"])
-        album = Album(artist, result["title"], result["year_released"], result["genre"], result["stock_qty"], result["purchase_price"], result["sell_price"], result["id"])
-        albums.append(album)
-    return albums
-
 def update(album):
     sql = """UPDATE albums SET (artist_id, title, year_released, genre, stock_qty, purchase_price, sell_price)
             = (%s, %s, %s, %s, %s, %s, %s) WHERE id = %s"""
@@ -61,3 +50,25 @@ def delete(id):
     sql = "DELETE FROM albums WHERE id = %s"
     values = [id]
     run_sql(sql, values)
+
+def filter_by_artist(id):
+    albums = []
+    sql = "SELECT * FROM albums WHERE artist_id = %s"
+    values = [id]
+    results = run_sql(sql, values)
+    for row in results:
+        artist = artist_repository.select(row["artist_id"])
+        album = Album(artist, row["title"], row["year_released"], row["genre"], row["stock_qty"], row["purchase_price"], row["sell_price"], row["id"])
+        albums.append(album)
+    return albums
+
+def markup(purchase_price, sell_price):
+    diff = abs(purchase_price - sell_price)
+    avg = (purchase_price + sell_price) / 2
+    return round((diff / avg) * 100)
+
+def total_albums_in_stock(albums):
+    total_albums = 0
+    for album in albums:
+        total_albums += album.stock_qty
+    return total_albums
