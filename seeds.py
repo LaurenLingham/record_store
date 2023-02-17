32a4f524ce9b599e84bfe13bfa7b7ea8bb97a73d
LaurenLingham <lauren.lingham@outlook.com>
2023-02-17 13:31:38 +0000

    added seeds file

---
+++ seeds.py
@@ -0,0 +1,9 @@
+from models.artist import Artist
+from models.album import Album
+
+import repositories.artist_repository as artist_repository
+import repositories.album_repository as album_repository
+
+album_repository.delete_all()
+artist_repository.delete_all()
+
