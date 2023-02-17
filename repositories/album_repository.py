ee34d00962b5c0fcaa0b8f92c0c50fa7f22e6cbf
LaurenLingham <lauren.lingham@outlook.com>
2023-02-17 11:54:43 +0000

    listed album functions

--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -4,4 +4,24 @@ from models.album import Album
 from models.artist import Artist
 import repositories.artist_repository as artist_repository
 
+
+def save(album):
+    pass
+
+def select_all():
+    pass
+
+def select(id):
+    pass
+
+def update(album):
+    pass
+
+def delete_all():
+    pass
+
+def delete(id):
+    pass
+
+
 Album(["artist_id"], ["title"], ["year_released"], ["genre"], ["stock_qty"], ["purchase_price"], ["sell_price"], ["id"])
