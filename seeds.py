c1b35233dd7ff48ca7c168dd537ba007b801cadd
LaurenLingham <lauren.lingham@outlook.com>
2023-02-17 13:38:20 +0000

    added artist & album variables

--- seeds.py
+++ seeds.py
@@ -7,3 +7,18 @@ import repositories.album_repository as album_repository
 album_repository.delete_all()
 artist_repository.delete_all()
 
+artist_1 = Artist("Biffy Clyro")
+artist_repository.save(artist_1)
+
+artist_2 = Artist("Brand New")
+artist_repository.save(artist_2)
+
+
+album_1 = Album(artist_1, "Opposites", 2018, "Rock", 10, 6.50, 11.99)
+album_repository.save(album_1)
+
+album_2 = Album(artist_1, "Blackened Sky", 2002, "Rock", 3, 5.00, 8.99)
+album_repository.save(album_2)
+
+album_3 = Album(artist_2, "The Devil and God Are Raging Inside Me", 2006, "Alt rock", 5, 6.00, 9.99)
+album_repository.save(album_3)
