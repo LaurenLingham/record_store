c2523e9970fbbf9ce457a1927a134a0837e831dc
LaurenLingham <lauren.lingham@outlook.com>
2023-02-17 15:28:50 +0000

    added route

--- controllers/store_controller.py
+++ controllers/store_controller.py
@@ -5,3 +5,8 @@ import repositories.album_repository as album_repository
 import repositories.artist_repository as artist_repository
 
 albums_blueprint = Blueprint("albums", __name__)
+
+@albums_blueprint.route("/albums")
+def albums():
+    albums = album_repository.select_all()
+    return render_template("albums/index.html")
