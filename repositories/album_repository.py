7a788585381840ee2be2f574efae93aa8779929d
LaurenLingham <lauren.lingham@outlook.com>
2023-02-17 11:50:45 +0000

    added album repo

---
+++ repositories/album_repository.py
@@ -0,0 +1,7 @@
+from db.run_sql import run_sql
+
+from models.album import Album
+from models.artist import Artist
+import repositories.artist_repository as artist_repository
+
+Album(["artist_id"], ["title"], ["year_released"], ["genre"], ["description"], ["stock_qty"], ["purchase_price"], ["sell_price"], ["id"])
