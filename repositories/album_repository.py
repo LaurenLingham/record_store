edb93a20f70b076fcde8616af2edf45932055f82
LaurenLingham <lauren.lingham@outlook.com>
2023-02-17 12:05:20 +0000

    defined save album function

--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -6,7 +6,13 @@ import repositories.artist_repository as artist_repository
 
 
 def save(album):
-    pass
+    sql = """INSERT INTO books (artist_id, title, year_released, genre, stock_qty, purchase_price, sell_price)
+            VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING *"""
+    values = [album.artist.id, album.title, album.year_released, album.genre, album.stock_qty, album.purchase_price, album.sell_price]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    album.id = id
+    return album
 
 def select_all():
     pass
