116f68edb842f38522f679826ba610e4608b26b2
LaurenLingham <lauren.lingham@outlook.com>
2023-02-17 11:39:51 +0000

    listed artist functions

--- respositories/artist_repository.py
+++ respositories/artist_repository.py
@@ -2,3 +2,24 @@ from db.run_sql import run_sql
 
 from models.artist import Artist
 from models.album import Album
+
+def save(artist):
+    pass
+
+def select_all():
+    pass
+
+def select(id):
+    pass
+
+def update(artist):
+    pass
+    
+def delete_all():
+    pass
+
+def delete(id):
+    pass
+
+def albums(artist):
+    pass
