8a987d3cd4ffadf1adc5a93ff4374a6f8b25dbf8
LaurenLingham <lauren.lingham@outlook.com>
2023-02-17 11:36:41 +0000

    added albums table

--- db/store_inventory.sql
+++ db/store_inventory.sql
@@ -2,3 +2,15 @@ CREATE TABLE artists (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255)
 );
+
+CREATE TABLE albums (
+    id SERIAL PRIMARY KEY,
+    artist INT NOT NULL REFERENCES artists(id),
+    title VARCHAR(255),
+    year_released YEAR,
+    genre VARCHAR(255),
+    description VARCHAR(255),
+    stock_qty INT,
+    purchase_price FLOAT,
+    sell_price FLOAT
+);
