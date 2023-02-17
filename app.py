42b2c34afd6a2adc20fc437a7300982e4778e8df
LaurenLingham <lauren.lingham@outlook.com>
2023-02-17 14:02:19 +0000

    created base html file

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from controllers.store_controller import albums_blueprint
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    pass
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
